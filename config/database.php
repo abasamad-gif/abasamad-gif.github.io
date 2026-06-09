ef6fb33dd45b0b4cac13db7438d60151fa78a1ba
abasamad-gif <abasazudin@gmail.com>
2026-06-09 14:50:38 +0800

    Force default database connection to pgsql with fallback URL

--- config/database.php
+++ config/database.php
@@ -85,7 +85,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL', postgresql:'//bnb_8a6p_user:R6I4lvtT7gYtw93C6tfD1NTvCOA7F2Qu@dpg-d8jhd80jo6nc73ecdbu0-a/bnb_8a6p'),
+            'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
